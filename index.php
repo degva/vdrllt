aa55075f90c442dccdcb97ad6e3b1db851fe0ec0
Diego Vargas <degva.29@gmail.com>
2014-07-26 23:52:37 -0500

    finito

--- index.php
+++ index.php
@@ -26,7 +26,13 @@
 			<div id='claim'>
 				<h1>VIDRILLETE™</h1>	
 				<h3>El cortador de botellas de vidrio perfecto ~</h3>
-				<p>Pasta ipsum dolor sit amet maccheroncelli stringozzi lanterne farfalle maltagliati. Ditalini stringozzi sagne 'ncannulate farfalle trenne fettuccine fagioloni vermicelli perciatelli. Campanelle pici creste di galli timpano trenette timpano spiralini fiorentine penne rigate pappardelle sacchettoni fedelini mafaldine vermicelloni torchio sorprese.</p>
+				<p>
+ Vidrillete™ es una máquina única en su género, es una curiosidad técnica basada en el uso de las propiedas térmicas del vidrio; creado por el Ing. José L. Vargas Monroy en 1986 y desarrollada cada día para mayor eficiencia y satisfacción de los usuarios.
+<br>
+Todas las versiones de Vidrillete™ pueden cortar botellas cualquiera que sea su perfil, ya sea circular, cuadrada, triangular,<br> hexagonal, rectangular, ovoidal. Incluso botellas de cualquier tipo de superficie, tales como las botellas lisas, con estrías,<br> con tallados, pavonadas y muchas mas. Todo esto incluye el poder de cortar botellas translúcidas, ámbares,<br> azules, negras, verdes, rojas, cristales muranos.
+<br>
+Con Vidrillete™ usted puede cortar la base de la botella, <span style='color:#fff'>su "cuello", solo el pico, etc, de acuerdo con las técnicas de corte que se describen</span> en el manual que viene incluido a su orden de compra.
+				</p>
 				<br>
 				<h4>Contacto</h4>
 				<p>Telf.: <b>(+51) 01 251 0209</b></p>
