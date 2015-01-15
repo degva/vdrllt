31b83f3f684ef4e455051fc3d1fc997187507166
Diego Vargas <degva.29@gmail.com>
2015-01-14 19:29:38 -0500

    Removed Everything

--- README.md
+++
@@ -1,1 +0,0 @@
-Vidrillete Web site Design
--- includes/footer.php
+++
@@ -1,4 +0,0 @@
-
-			<div id='footer'>
-				<div style='color: #000;float:left; padding: 2px 0 0 10px;'>Vidrillete™ | Todos los derechos reservados - 2014</div>	
-			</div>
